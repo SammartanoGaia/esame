a79a63248a50794a87b0fa3da6d88443d6fcc001
SammartanoGaia <165411784+SammartanoGaia@users.noreply.github.com>
2024-06-14 11:48:07 +0200

    z

---
+++ script.js
@@ -0,0 +1,75 @@
+// script.js
+function openPopup(overlay) {
+    document.getElementById('overlay').style.display = 'flex';
+}
+
+function closePopup(overlay) {
+    document.getElementById('overlay').style.display = 'none';
+}
+
+
+window.onclick = function(event) {
+    let popups = document.getElementsByClassName('popup');
+    for (let i = 0; i < popups.length; i++) {
+        if (event.target == popups[i]) {
+            popups[i].style.display = 'none';
+        }
+    }
+}
+
+
+
+function openPopup1(overlay1) {
+    document.getElementById('overlay1').style.display = 'flex';
+}
+
+function closePopup1(overlay1) {
+    document.getElementById('overlay1').style.display = 'none';
+}
+
+function openPopup2(overlay2) {
+    document.getElementById('overlay2').style.display = 'flex';
+}
+
+function closePopup2(overlay2) {
+    document.getElementById('overlay2').style.display = 'none';
+}
+
+
+
+function openPopup3(overlay3) {
+    document.getElementById('overlay3').style.display = 'flex';
+}
+
+function closePopup3(overlay3) {
+    document.getElementById('overlay3').style.display = 'none';
+}
+
+
+
+
+function openPopup4(overlay4) {
+    document.getElementById('overlay4').style.display = 'flex';
+}
+
+function closePopup4(overlay4) {
+    document.getElementById('overlay4').style.display = 'none';
+}
+
+
+function openPopup5(overlay5) {
+    document.getElementById('overlay5').style.display = 'flex';
+}
+
+function closePopup5(overlay5) {
+    document.getElementById('overlay5').style.display = 'none';
+}
+
+
+function openPopup6(overlay6) {
+    document.getElementById('overlay6').style.display = 'flex';
+}
+
+function closePopup6(overlay6) {
+    document.getElementById('overlay6').style.display = 'none';
+}
