d55ba7ac1f756f0eb08e1e349040e51aaa64973e
SammartanoGaia <165411784+SammartanoGaia@users.noreply.github.com>
2024-06-25 19:54:36 +0200

    Add files via upload

---
+++ script3.js
@@ -0,0 +1,27 @@
+// script.js
+function openPopup(overlay) {
+    document.getElementById('overlay').style.display = 'flex';
+}
+
+function close2Popup(overlay) {
+    document.getElementById('overlay').style.display = 'none';
+}
+
+
+function openPopup1(overlay1) {
+    document.getElementById('overlay1').style.display = 'flex';
+}
+
+function close2Popup1(overlay1) {
+    document.getElementById('overlay1').style.display = 'none';
+}
+
+
+
+function openPopup2(overlay2) {
+    document.getElementById('overlay2').style.display = 'flex';
+}
+
+function close2Popup2(overlay2) {
+    document.getElementById('overlay2').style.display = 'none';
+}
